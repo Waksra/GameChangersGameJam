08074f3623d79a4ad50988d3f78b0b09c0a9bf52
Wiktor Ravndal <w.ravndal@gmail.com>
2020-11-22 13:45:00 +0100

    Moare stuff

---
+++ RatGame/Assets/Scripts/TimedPesantSpawning.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TimedPesantSpawning : MonoBehaviour
+{
+	public Vector2 minMaxTimeBetween = new Vector2(2, 5);
+
+	public GameObject objectToSpawn;
+
+	private IEnumerator TimedSpawn()
+	{
+		yield return new WaitForSeconds(Random.Range(minMaxTimeBetween.x, minMaxTimeBetween.y));
+
+		ObjectPoolManager.GetPooledObject(objectToSpawn, transform.position, Quaternion.identity);
+	}
+}
